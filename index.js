4a509f66a2ea6946807accf5f28ad12465ec03a5
Raj Kale <kale44772@gmail.com>
2021-03-02 23:20:26 +0530

    Highway Animation

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-()=>{
-    console.log("Running")
-}
+import Highway from '@dogstudio/highway';
+import Fade from './transition'
+
+const H=new Highway.Core({
+    // home:Fade,
+    // about:Slide
+    transitions:{
+        default:Fade
+    }
+})
